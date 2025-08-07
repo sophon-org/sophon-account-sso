e4c96a85aa00a21c8bc02393aa85621452ac9370
Israel Crisanto <me@israelcrisanto.com>
2025-08-07 08:59:10 +0100

    fix: updating dynamic hooks

--- apps/account-server/src/providers/Web3Provider.tsx
+++ apps/account-server/src/providers/Web3Provider.tsx
@@ -1,13 +1,14 @@
-'use client';
-import { EthereumWalletConnectors } from '@dynamic-labs/ethereum';
-import { DynamicContextProvider } from '@dynamic-labs/sdk-react-core';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import type { ReactNode } from 'react';
-import { createConfig, http, WagmiProvider } from 'wagmi';
-import { injected } from 'wagmi/connectors';
-import { env } from '@/env';
-import { sendMessage } from '@/events';
-import { VIEM_CHAIN } from '@/lib/constants';
+"use client";
+import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
+import { DynamicContextProvider } from "@dynamic-labs/sdk-react-core";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import type { ReactNode } from "react";
+import { createConfig, http, WagmiProvider } from "wagmi";
+import { injected } from "wagmi/connectors";
+import { env } from "@/env";
+import { sendMessage } from "@/events";
+import { VIEM_CHAIN } from "@/lib/constants";
+import { dynamicEvents } from "@dynamic-labs/sdk-react-core";
 
 // Wagmi config with MetaMask connector - uses environment-based chain
 const wagmiConfig = createConfig({
@@ -27,56 +28,43 @@ const queryClient = new QueryClient({
   },
 });
 
+// work around for dynamic events not working properly and giving the primary wallet on auth success
+dynamicEvents.on("primaryWalletChanged", (newPrimaryWallet) => {
+  console.log("primaryWalletChanged was called", newPrimaryWallet);
+  sendMessage("k1.login", {
+    address: newPrimaryWallet.address as `0x${string}`,
+    wallet: newPrimaryWallet,
+  });
+});
+
 export const Web3Provider = ({ children }: { children: ReactNode }) => {
   return (
     <DynamicContextProvider
       settings={{
-        initialAuthenticationMode: 'connect-and-sign',
+        initialAuthenticationMode: "connect-and-sign",
         environmentId: env.NEXT_PUBLIC_DYNAMIC_PROVIDER_ID,
         walletConnectors: [EthereumWalletConnectors],
         events: {
           // This is called when the user clicks on the login button
           onAuthInit() {
-            console.log('onAuthInit');
-            sendMessage('k1.login.init', null);
+            console.log("onAuthInit");
+            sendMessage("k1.login.init", null);
           },
           // This is called on the callback processing from social login
           onAuthFlowOpen() {
-            console.log('onAuthFlowOpwn');
-            sendMessage('k1.login.init', null);
-          },
-          onEmbeddedWalletCreated(credential, user) {
-            console.log('🔥 onEmbeddedWalletCreated', user, credential);
-          },
-          onWalletAdded(user) {
-            console.log('onWalletAdded');
-            sendMessage('k1.login', {
-              address: user.wallet.address as `0x${string}`,
-              wallet: user.wallet,
-            });
-          },
-          // This is called after everything is validated and the user should be considered authenticated
-          onAuthSuccess(user) {
-            console.log('onAuthSuccess', user);
-            if (user.primaryWallet) {
-              sendMessage('k1.login', {
-                address: user.primaryWallet!.address as `0x${string}`,
-                wallet: user.primaryWallet!,
-              });
-            }
+            console.log("onAuthFlowOpwn");
+            sendMessage("k1.login.init", null);
           },
           // This is called when the user
           onLogout() {
-            console.log('onAuthSuccess');
-            sendMessage('k1.logout', null);
+            console.log("onAuthSuccess");
+            sendMessage("k1.logout", null);
           },
         },
       }}
     >
       <WagmiProvider config={wagmiConfig}>
-        <QueryClientProvider client={queryClient}>
-          {children}
-        </QueryClientProvider>
+        <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
       </WagmiProvider>
     </DynamicContextProvider>
   );
