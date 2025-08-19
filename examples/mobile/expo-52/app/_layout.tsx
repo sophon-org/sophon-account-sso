c66a70ff8e61951caedcdf447db34b06e95f2f25
Israel Crisanto <me@israelcrisanto.com>
2025-08-19 15:47:14 +0100

    chore: adding example app for expo-52

---
+++ examples/mobile/expo-52/app/_layout.tsx
@@ -0,0 +1,39 @@
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from '@react-navigation/native';
+import { SophonContextProvider } from '@sophon-labs/account-react-native';
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import 'react-native-reanimated';
+
+import { useColorScheme } from '@/hooks/useColorScheme';
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+
+  if (!loaded) {
+    // Async font loading only occurs in development.
+    return null;
+  }
+
+  return (
+    <SophonContextProvider
+      network={'testnet'}
+      partnerId="123b216c-678e-4611-af9a-2d5b7b061258"
+    >
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+        <StatusBar style="auto" />
+      </ThemeProvider>
+    </SophonContextProvider>
+  );
+}
