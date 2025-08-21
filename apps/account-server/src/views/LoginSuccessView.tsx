d319c37c8a68b9d1367c299dce375a367bce7aa2
Israel Crisanto <me@israelcrisanto.com>
2025-08-21 16:02:54 +0100

    chore: adding copy button to account address

--- apps/account-server/src/views/LoginSuccessView.tsx
+++ apps/account-server/src/views/LoginSuccessView.tsx
@@ -1,14 +1,42 @@
 import { shortenAddress } from '@sophon-labs/account-core';
+import { useState } from 'react';
+import { IconCopy } from '@/components/icons/icon-copy';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
 import VerificationImage from '@/components/ui/verification-image';
 import { useAccountContext } from '@/hooks/useAccountContext';
 
 export default function LoginSuccessView() {
   const { account } = useAccountContext();
+  const [isCopied, setIsCopied] = useState(false);
+
+  function copyAddressToClipboard() {
+    navigator.clipboard.writeText(account!.address);
+    setIsCopied(true);
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 1000);
+  }
+
   return (
     <div className="flex flex-col items-center justify-center gap-8 mt-6 flex-grow">
       <VerificationImage image="/images/avatar-example.png" />
-
-      <h5 className="text-lg font-bold">{shortenAddress(account?.address)}</h5>
+      <div className="flex flex-row items-center gap-2">
+        <h5 className="text-lg font-bold">
+          {shortenAddress(account?.address)}
+        </h5>
+        <Tooltip delayDuration={0} open={isCopied}>
+          <TooltipTrigger asChild>
+            <button type="button" onClick={copyAddressToClipboard}>
+              <IconCopy className="cursor-pointer w-4 h-4" />
+            </button>
+          </TooltipTrigger>
+          <TooltipContent>Copied!</TooltipContent>
+        </Tooltip>
+      </div>
     </div>
   );
 }
