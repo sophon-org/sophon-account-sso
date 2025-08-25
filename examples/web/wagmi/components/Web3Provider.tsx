5bbaebe7acb1c83064bca071cd27ab7f469be46a
consvic <cocoy_shuffle@me.com>
2025-08-25 14:35:03 -0600

    fix: lint

--- examples/web/wagmi/components/Web3Provider.tsx
+++ examples/web/wagmi/components/Web3Provider.tsx
@@ -1,18 +1,18 @@
-"use client";
+'use client';
 
-import { createSophonEIP6963Emitter } from "@sophon-labs/account-eip6963";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import type { ReactNode } from "react";
-import { sophonTestnet } from "viem/chains";
-import { createConfig, http, type State, WagmiProvider } from "wagmi";
+import { createSophonEIP6963Emitter } from '@sophon-labs/account-eip6963';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import type { ReactNode } from 'react';
+import { sophonTestnet } from 'viem/chains';
+import { createConfig, http, type State, WagmiProvider } from 'wagmi';
 
-createSophonEIP6963Emitter("testnet");
+createSophonEIP6963Emitter('testnet');
 
-export const projectId = "760fb7a448e58431c9cfbab80743ab1c";
+export const projectId = '760fb7a448e58431c9cfbab80743ab1c';
 
 const queryClient = new QueryClient();
 
-if (!projectId) throw new Error("Project ID is not defined");
+if (!projectId) throw new Error('Project ID is not defined');
 
 const config = createConfig({
   chains: [sophonTestnet],
