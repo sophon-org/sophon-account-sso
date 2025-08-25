2267888e42a30526d9a6a18994e61933a2c6914a
consvic <cocoy_shuffle@me.com>
2025-08-25 14:30:08 -0600

    fix: wagmi example emitter and deps

--- examples/web/wagmi/components/Web3Provider.tsx
+++ examples/web/wagmi/components/Web3Provider.tsx
@@ -1,35 +1,25 @@
-'use client';
+"use client";
 
-import { createSophonEIP6963Emitter } from '@sophon-labs/account-eip6963';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ConnectKitProvider, getDefaultConfig } from 'connectkit';
-import type { ReactNode } from 'react';
-import { sophonTestnet } from 'viem/chains';
-import { createConfig, http, type State, WagmiProvider } from 'wagmi';
+import { createSophonEIP6963Emitter } from "@sophon-labs/account-eip6963";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import type { ReactNode } from "react";
+import { sophonTestnet } from "viem/chains";
+import { createConfig, http, type State, WagmiProvider } from "wagmi";
 
-createSophonEIP6963Emitter('testnet', 'http://localhost:3000');
+createSophonEIP6963Emitter("testnet");
 
-export const projectId = '760fb7a448e58431c9cfbab80743ab1c';
+export const projectId = "760fb7a448e58431c9cfbab80743ab1c";
 
 const queryClient = new QueryClient();
 
-if (!projectId) throw new Error('Project ID is not defined');
+if (!projectId) throw new Error("Project ID is not defined");
 
-const config = createConfig(
-  getDefaultConfig({
-    chains: [sophonTestnet],
-    transports: {
-      [sophonTestnet.id]: http(),
-    },
-
-    walletConnectProjectId: projectId,
-
-    appName: 'Sophon Account via EIP-6963',
-    appDescription: 'Sophon Account via EIP-6963',
-    appUrl: 'https://my.staging.sophon.xyz',
-    appIcon: 'https://family.co/logo.png',
-  }),
-);
+const config = createConfig({
+  chains: [sophonTestnet],
+  transports: {
+    [sophonTestnet.id]: http(),
+  },
+});
 
 export default function Web3ModalProvider({
   children,
@@ -40,9 +30,7 @@ export default function Web3ModalProvider({
 }) {
   return (
     <WagmiProvider config={config} initialState={initialState}>
-      <QueryClientProvider client={queryClient}>
-        <ConnectKitProvider>{children}</ConnectKitProvider>
-      </QueryClientProvider>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </WagmiProvider>
   );
 }
