264a83651c64683d1b725e75d5094200812f76ee
Israel Crisanto <me@israelcrisanto.com>
2025-09-09 14:26:12 +0100

    feat(SOP-1553): replacing raw svg by phosphor icon

--- apps/account-server/src/components/ui/offline-content.tsx
+++ apps/account-server/src/components/ui/offline-content.tsx
@@ -1,9 +1,9 @@
-import { IconOffline } from '../icons/icon-offline';
+import { LinkBreakIcon } from '@phosphor-icons/react';
 
 export const OfflineContent = () => {
   return (
     <div className="flex flex-col justify-center text-center items-center">
-      <IconOffline className="mt-4" />
+      <LinkBreakIcon className="mt-4" size={32} />
       <h1 className="font-bold text-lg mt-4">You are offline!</h1>
       <h2 className="text-xs text-gray-700">Please try again later.</h2>
     </div>
