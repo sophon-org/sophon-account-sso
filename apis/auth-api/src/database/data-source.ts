d7d195c569b17e5546ece02f2ecab1241a02be04
Roman Hiden <romeo8881@gmail.com>
2025-09-11 12:32:27 +0300

    move to typeorm

---
+++ apis/auth-api/src/database/data-source.ts
@@ -0,0 +1,11 @@
+import { DataSource } from "typeorm";
+import { Session } from "../sessions/session.entity";
+
+export default new DataSource({
+  type: "postgres",
+  url: process.env.DATABASE_URL,
+  entities: [Session],
+  migrations: ["src/migrations/*.ts"],     // use dist/*.js in prod
+  migrationsTableName: "typeorm_migrations",
+  synchronize: false,
+});
