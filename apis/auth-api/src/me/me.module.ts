3b797d2e82bb15d1c19be88d7200b20c5e536235
Roman Hiden <romeo8881@gmail.com>
2025-10-06 16:48:54 +0300

    more tests + lint

--- apis/auth-api/src/me/me.module.ts
+++ apis/auth-api/src/me/me.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
+import { AuthModule } from "src/auth/auth.module";
 import { ConsentsModule } from "src/consents/consents.module";
 import { HyperindexModule } from "src/hyperindex/hyperindex.module";
 import { ConsentController } from "./consent.controller";
 import { K1OwnerController } from "./k1-owner.controller";
 
 @Module({
-	imports: [ConsentsModule, HyperindexModule],
+	imports: [ConsentsModule, HyperindexModule, AuthModule],
 	controllers: [ConsentController, K1OwnerController],
 })
 export class MeModule {}
