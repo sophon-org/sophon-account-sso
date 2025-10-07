87d7391d211b97cdc1b8e83f438dfa43d2fc6618
Israel Crisanto <me@israelcrisanto.com>
2025-10-07 17:22:43 +0100

    fix(SOP-1738): syncing wagmi connection status for clients that are mixing with wagmi hooks

---
+++ packages/account-react/src/wagmi/wagmi-sync-sophon.tsx
@@ -0,0 +1,58 @@
+import { useCallback, useEffect, useRef } from 'react';
+import { type Connector, useConnect, useDisconnect } from 'wagmi';
+import { useSophonAccount } from '../hooks';
+
+export interface WagmiSophonSyncProps {
+  children: React.ReactNode;
+  connector: Connector;
+}
+
+export const WagmiSophonSync = ({
+  children,
+  connector,
+}: WagmiSophonSyncProps) => {
+  const { account } = useSophonAccount();
+  const { connect } = useConnect();
+  const { disconnect } = useDisconnect();
+  const lastConnectedAddress = useRef(undefined);
+
+  const wagmiDisconnect = useCallback(() => {
+    disconnect();
+    lastConnectedAddress.current = null;
+  }, [disconnect]);
+
+  const wagmiConnect = useCallback(
+    (connector: Connector, walletId: string) => {
+      lastConnectedAddress.current = walletId;
+      connect({ connector });
+    },
+    [connect],
+  );
+
+  const sync = useCallback(() => {
+    if (!account?.address || !connector) {
+      wagmiDisconnect();
+      return;
+    }
+
+    const walletId = `${connector.id}-${account.address}`;
+    // nothing new, just ignore
+    if (walletId === lastConnectedAddress.current) {
+      return;
+    }
+
+    // other wallet connected, then disconnect
+    if (lastConnectedAddress.current) {
+      wagmiDisconnect();
+    }
+
+    wagmiConnect(connector, walletId);
+  }, [account, connector, wagmiDisconnect, wagmiConnect]);
+
+  useEffect(() => {
+    console.debug(`Syncing sophon account with wagmi: ${account?.address}`);
+    sync();
+  }, [sync, account]);
+
+  return <>{children}</>;
+};
