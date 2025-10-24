8220e4f10c0fea01567ab8b96fa173e75e4286e4
Cleanderson Lobo <cleandersonlobo@gmail.com>
2025-10-23 22:44:15 -0300

    feat(account-react-native): i18n provider

---
+++ packages/account-react-native/src/i18n/i18n-provider.tsx
@@ -0,0 +1,102 @@
+import {
+  createContext,
+  type PropsWithChildren,
+  useContext,
+  useMemo,
+} from 'react';
+import en from './locales/en.json';
+import es from './locales/es.json';
+
+type PathImpl<T, Key extends keyof T> = Key extends string
+  ? T[Key] extends Record<string, any>
+    ?
+        | `${Key}.${PathImpl<T[Key], Exclude<keyof T[Key], keyof any[]>> & string}`
+        | `${Key}`
+    : `${Key}`
+  : never;
+
+type Path<T> = PathImpl<T, keyof T> | keyof T;
+
+type TranslationKeys = Path<typeof en>;
+
+/**
+ * Locale code (ISO 639-1 or BCP-47).
+ * Examples:
+ *  - "en" (English, default)
+ */
+export type SupportedLocaleCode = 'en' | 'es' | string;
+
+export const defaultResources: Record<SupportedLocaleCode, typeof en> = {
+  en,
+  es,
+};
+
+export type TranslationFunction = (
+  key: TranslationKeys,
+  variables?: Record<string, string | number>,
+) => string;
+
+export interface LocalizationProviderProps {
+  /** Locale code for translations (default: "en") */
+  locale?: SupportedLocaleCode;
+}
+
+export interface LocalizationContextProps extends LocalizationProviderProps {
+  t: TranslationFunction;
+}
+
+const TranslationContext = createContext<LocalizationContextProps>({
+  locale: 'en',
+  t: (key) => key,
+});
+
+export const useTranslation = () => useContext(TranslationContext);
+
+/**
+ * Provider for handling translations inside the library.
+ */
+export function LocalizationProvider({
+  locale = 'en',
+  children,
+}: PropsWithChildren<LocalizationProviderProps>) {
+  const dict = useMemo(
+    () => defaultResources[locale || ''] || defaultResources['en'],
+    [locale],
+  );
+
+  const t: TranslationFunction = useMemo(
+    () => (key, variables) => {
+      const translation = key
+        .split('.')
+        .reduce<string | Record<string, unknown>>(
+          (acc, part) => {
+            if (acc && typeof acc === 'object' && part in acc) {
+              return acc[part] as string | Record<string, unknown>;
+            }
+            return key;
+          },
+          dict as Record<string, unknown>,
+        );
+
+      let result = typeof translation === 'string' ? translation : key;
+
+      if (variables) {
+        for (const [varKey, varValue] of Object.entries(variables)) {
+          result = result.replace(
+            new RegExp(`{{${varKey}}}`, 'g'),
+            String(varValue),
+          );
+        }
+      }
+
+      return result;
+    },
+    [dict],
+  );
+
+  return (
+    <TranslationContext.Provider value={{ locale, t }}>
+      {children}
+    </TranslationContext.Provider>
+  );
+}
