0d7e269874afc4fe07a01bec05e99c06a9f717b2
Israel Crisanto <me@israelcrisanto.com>
2025-11-03 13:07:26 +0000

    chore: adding prebuild example

---
+++ examples/mobile/expo-54-prebuild/app/_layout.tsx
@@ -0,0 +1,43 @@
+// import { polyfillWebCrypto } from 'expo-standard-web-crypto';
+// import 'text-encoding-polyfill';
+// import { randomUUID } from 'expo-crypto';
+
+// polyfillWebCrypto();
+// // @ts-ignore
+// crypto.randomUUID = randomUUID;
+// //
+
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import 'react-native-reanimated';
+import './global.css';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
+import { ThemeProvider } from '@/lib/theme-context';
+import { Web3Provider } from '@/providers/Web3Provider';
+
+export default function RootLayout() {
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+
+  if (!loaded) {
+    // Async font loading only occurs in development.
+    return null;
+  }
+
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Web3Provider>
+        <ThemeProvider defaultTheme="system">
+          <Stack>
+            <Stack.Screen name="(screens)" options={{ headerShown: false }} />
+            <Stack.Screen name="+not-found" />
+          </Stack>
+          <StatusBar style="auto" />
+        </ThemeProvider>
+      </Web3Provider>
+    </GestureHandlerRootView>
+  );
+}
