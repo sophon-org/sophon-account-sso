d17f3aad8b38771933a82af74091b2a32b8bf17c
Israel Crisanto <me@israelcrisanto.com>
2025-11-03 13:46:15 +0000

    chore: adding reactive webview to sort logout login problems

--- examples/mobile/expo-54-prebuild/lib/dynamic.client.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@dynamic-labs/client';
-import { ReactNativeExtension } from '@dynamic-labs/react-native-extension';
-
-export const dynamicClient = createClient({
-  environmentId: '767555fd-deac-4852-bdf2-ec4442697ea7',
-}).extend(ReactNativeExtension());
