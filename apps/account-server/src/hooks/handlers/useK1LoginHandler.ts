0992ae31a7e100edfe93ff85248b252543e6299e
Israel Crisanto <israel@sophon.xyz>
2025-12-09 14:23:03 +0000

    chore: making stuff work

--- apps/account-server/src/hooks/handlers/useK1LoginHandler.ts
+++ apps/account-server/src/hooks/handlers/useK1LoginHandler.ts
@@ -6,6 +6,7 @@ import { useEventHandler } from '@/events/hooks';
 import { useAccountContext } from '@/hooks/useAccountContext';
 import { SOPHON_VIEM_CHAIN } from '@/lib/constants';
 import { getsSmartAccounts } from '@/service/smart-account.server';
+import { serverLog } from '@/lib/server-log';
 
 /**
  * Handles every login request from k1, being possible right now
@@ -19,21 +20,28 @@ export const useK1LoginHandler = () => {
   const { login, setSmartAccountDeployed } = useAccountContext();
 
   useEventHandler('k1.login', async (payload) => {
+    console.log('🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥 k1.login', payload);
     if (!payload.address) {
+      serverLog(`🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥 k1.login ignored`);
       return;
     }
 
     actorRef.send({ type: 'ACCOUNT_AUTHENTICATED' });
     const accounts = await getsSmartAccounts(payload.address);
+    serverLog(`🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥 connected accounts ${JSON.stringify(accounts)}`);
 
     let smartAccountAddress: `0x${string}`;
     if (accounts.length === 0) {
+      console.log(`🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥 deploying smart account for ${payload.address}`);
+      serverLog(`deploying smart account for ${payload.address}`);
       const response = await AuthService.deploySmartAccount(
         SOPHON_VIEM_CHAIN.id as ChainId,
         payload.address,
       );
       smartAccountAddress = response.contracts[0];
     } else {
+      console.log(`🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥 using existing smart account ${accounts[0]}`);
+      serverLog(`using existing smart account ${accounts[0]}`);
       smartAccountAddress = accounts[0];
     }
 
